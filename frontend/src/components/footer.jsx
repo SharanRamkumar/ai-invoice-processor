1b413acc4a0b20a9d4940494690f3af393608528
Sharan R <sharanrs1205@gmail.com>
2026-08-13 12:21:57 +0530

    Add Footer component to display copyright notice

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+
+    return (
+
+        <footer className="text-center py-6 text-gray-500">
+
+            AI Invoice Processor © 2026
+
+        </footer>
+
+    );
+
+}
