c29202817473b969f929abf44150b2c76f8b88b7
Sharan R <sharanrs1205@gmail.com>
2026-09-22 15:52:41 +0530

    Update chatbot.jsx

--- frontend/src/components/chatbot.jsx
+++ frontend/src/components/chatbot.jsx
@@ -25,7 +25,8 @@ function Chatbot() {
         setLoading(true);
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/chat", {
+            const API_URL = import.meta.env.VITE_API_URL
+            const response = await fetch(`${API_URL}/chat`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ question: userMessage })
